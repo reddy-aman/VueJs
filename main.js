c2c6dbc6ef37ea4f0e4f5f223e882ce14b7dcb8b
aman-reddy <reddyaman@sameer.ac.in>
2024-05-06 11:15:25 +0530

    updated code with notes

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const ProductInfo = Vue.createApp({
     data() {
         return {
+            cart:0,
             product_Name:'Socks',
-            onSale :false,
-            inventary : 8,
+            onSale :false, // Conditional Rendering
+            inventary : 8, // Conditional Rendering
             About :'About Product :',
-            disc : ['50% Cotton','30% wool','20% Polyester'],
-            url :'https://www.myntra.com/sock',
-            image :'./assets/images/socks_green.jpg'
-
+            disc : ['50% Cotton','30% wool','20% Polyester'], //list Rendering
+            url :'https://www.myntra.com/sock', // Attribute Binding
+            image :'./assets/images/socks_green.jpg', // Attribute Binding
+            variants: [
+      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+      { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+    ],
         }
     },
+    methods:{
+        addToCart(){
+            this.cart += 1  // Event Handling using function 
+        },
+        updateImage(variantImage) {
+    this.image = variantImage
+  }
+    }
 })
 
 // const amanapp = Vue.createApp({
